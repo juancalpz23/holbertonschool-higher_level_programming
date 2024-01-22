fc1c2f2a42dab1736ee56aeb1d9b8be2a87c552e
Juan C Lopez <juancalpz@yahoo.com>
2024-01-22 14:16:23 -0400

    Fixing checkers

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,7 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     print("{} argument:".format(len(sys.argv) - 1))
 else:
-    print("{} aguments:".format(len(sys.argv) - 1))
+    print("{} arguments:".format(len(sys.argv) - 1))
+
 for a in range(1, len(sys.argv)):
     print("{}: {}".format(a, sys.argv[a]))
