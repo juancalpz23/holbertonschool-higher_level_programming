84af3d85c865512cdfdbfeaffeaf11fdc9febed8
Juan C Lopez <juancalpz@yahoo.com>
2024-01-29 13:16:47 -0400

    Checkers

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/bin/usr/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for arr in my_list:
-        if arr == search:
+    for ar in my_list:
+        if ar == search:
             new_list.append(replace)
         else:
-            new_list.append(arr)
+            new_list.append(ar)
     return new_list
