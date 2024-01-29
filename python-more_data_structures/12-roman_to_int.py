06614558c066df2591aa805e0ff3b4b8ef752b8e
Juan C Lopez <juancalpz@yahoo.com>
2024-01-29 17:17:36 -0400

    Added 12-roman_to_int.py file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if roman_string and type(roman_string) is str:
         numerals = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
         value = 0
-        for a, x in zip(roman_string,roman_string[1:]):
+        for a, x in zip(roman_string, roman_string[1:]):
             if numerals[a] < numerals[x]:
                 value -= numerals[a]
             else:
