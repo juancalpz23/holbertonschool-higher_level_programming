ce04feff195b222c0e8abf4453fb08ff0525752a
Juan C Lopez <juancalpz@yahoo.com>
2024-01-31 15:53:10 -0400

    Checkers

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
     def size(self):
         return self.__size
     
-    @property.setter
+    @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
