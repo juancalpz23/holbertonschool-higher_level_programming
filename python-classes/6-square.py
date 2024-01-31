b4c16075ab3e8fe9ce715e8348352c8add58d7a8
Juan C Lopez <juancalpz@yahoo.com>
2024-01-31 17:19:41 -0400

    Added file 6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""""
+Square class definition
+"""
+
+
+class Square:
+    """"
+    Square initialization with privite instance attribute size
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value[0]) is not int or type(value[1]) is not int or
+                value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    """
+    Define the square area
+    """
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            count = 1
+            while (count <= self.size):
+                if count == 1:
+                    print("\n" * self.__position[1], end="")
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+                count += 1
