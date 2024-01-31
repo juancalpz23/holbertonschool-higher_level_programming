af5f278d20d5491901c991e42472c3b859a13c54
Juan C Lopez <juancalpz@yahoo.com>
2024-01-31 15:34:13 -0400

    Pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,6 +8,7 @@ class Square:
     Square initialization with privite instance attribute size
     """
 
+
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
