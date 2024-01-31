44c16ebfeef52077154d0d87b9bf31a7669d5a23
Juan C Lopez <juancalpz@yahoo.com>
2024-01-31 14:37:57 -0400

    Added file - 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""""
+Square class definition
+"""
+
+
+class Square:
+    """"
+    Square with privite instance attribute size
+    """
+
+    def __init__(self, size):
+        self.__size = size        
