356c98deffa9bf49a88edba46c95f8131bf2e6ba
Juan C Lopez <juancalpz@yahoo.com>
2024-01-31 15:49:39 -0400

    Added file 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""""
+Square class definition
+"""
+
+
+class Square:
+    """"
+    Square initialization with privite instance attribute size
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @property.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """
+    Define the square area
+    """
+
+    def area(self):
+        return self.__size * self.__size
