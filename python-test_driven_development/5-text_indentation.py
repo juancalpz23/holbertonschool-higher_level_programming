0ae57a309ddddc8f272ca10f9e79b7d0ce7ae31b
Juan C Lopez <juancalpz@yahoo.com>
2024-02-01 15:34:36 -0400

    Added files 5-text_indentation.py and 5-text_indentation.txt

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Prints text
+"""
+
+
+def text_indentation(text):
+    """
+    Function prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    txt_len = len(text)
+    while i < txt_len:
+        if text[i] in ('.', '?', ':'):
+            print("{}\n".format(text[i]))
+            while i + 1 < txt_len and text[i + 1] == " ":
+                i += 1
+        else:
+            print(text[1], end='')
+        i += 1
