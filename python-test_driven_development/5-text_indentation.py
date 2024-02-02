f964c149adb31f72a5ff424e687863049320186e
Juan C Lopez <juancalpz@yahoo.com>
2024-02-02 14:46:32 -0400

    checkers

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
 """
-Prints text
+Prints text with 2 newlines after
+character - . ? :
 """
 
 
 def text_indentation(text):
     """
-    Function prints a text with 2 new lines
-    after each of these characters: ., ? and :
+    function prints text and 2 newlines
     """
 
-    if isinstance(text, str):
-        raise TypeError("text must be a string")
-    i = 0
-    txt_len = len(text)
-    while i < txt_len:
-        if text[i] in ('.', '?', ':'):
-            print("{}\n".format(text[i]))
-            while i + 1 < txt_len and text[i + 1] == " ":
-                i += 1
-        else:
-            print(text[1], end='')
-        i += 1
+    if text is None or not isinstance(text, str) or len(text) < 0:
+        raise TypeError('text must be a string')
+    string = "".join([a if a not in ".?:" else a + "\n\n" for a in text])
+    print("\n".join([x.strip() for x in string.split("\n")]), end="")
