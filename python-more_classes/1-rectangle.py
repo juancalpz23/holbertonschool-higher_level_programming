af5b787ac5b97b6c09d2d5c415d95cd76f65782e
Juan C Lopez <juancalpz@yahoo.com>
2024-02-05 10:03:02 -0400

    Missing values

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -30,6 +30,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
         
         def height(self):
             """
@@ -45,3 +46,4 @@ class Rectangle:
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height must be >= 0")
+            self.__height = value
