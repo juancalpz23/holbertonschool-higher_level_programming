1710980400e859ce314dae1b2bb8fa35ae0a9eec
Juan C Lopez <juancalpz@yahoo.com>
2024-02-05 18:19:43 -0400

    Added file 3-rectangle.py

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+Class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Define a Rectangle with withd and height
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Width getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter the value of width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter the height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter the value of height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Return to the rectangle area
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        Return to rectangle perimeter
+        """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        """
+        Prints rectangle using #
+        or o if empty string
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        width = "#" * self.width
+        for i in range(self.height - 1):
+            rectangle += "\n" + width
+        return (rectangle)
