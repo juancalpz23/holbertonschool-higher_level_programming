0ef2a434403d8f529a79f538042f3ac0d8a48ae5
Juan C Lopez <juancalpz@yahoo.com>
2024-02-05 20:57:10 -0400

    Pycodestyle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -97,9 +97,9 @@ class Rectangle:
         Return the biggest rectngle
         """
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle" )
+            raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle" )
+            raise TypeError("rect_2 must be an instance of Rectangle")
         elif rect_1.area() < rect_2.area():
             return rect_2
         return rect_1
