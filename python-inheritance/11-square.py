a8a977dceb4271a890ff152813b7e91e225abbfb
Juan C Lopez <juancalpz@yahoo.com>
2024-02-07 17:59:12 -0400

    Pycodestyle

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,20 +13,20 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-            """
-            Class instantiation
-            """
+        """
+        Class instantiation
+        """
 
-            self.integer_validator("size", size)
-            self.__size = size
-            super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-          """
-          Fucntion to calculate the area of the square
-          """
+        """
+        Fucntion to calculate the area of the square
+        """
 
-          return self.__size * self.__size
+        return self.__size * self.__size
 
     def __str__(self):
         """
