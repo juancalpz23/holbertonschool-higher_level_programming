9e27d82aa22299915f6cc122c1eeb0fda0959c82
Juan C Lopez <juancalpz@yahoo.com>
2024-02-09 09:09:44 -0400

    Added file 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student
+"""
+
+
+class Student:
+    """
+    The attributes for the class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Class instantiation
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation
+        of a Student instance
+        """
+
+        return (self.__dict__)
