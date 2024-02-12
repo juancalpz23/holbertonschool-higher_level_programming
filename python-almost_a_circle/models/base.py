00c8aaed6b2cb539cc52759154892aaeef4f6238
Juan C Lopez <juancalpz@yahoo.com>
2024-02-12 10:55:11 -0400

    Added file base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Base():
+    """
+    Class Base with private attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Instantiation of class which checks for id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
