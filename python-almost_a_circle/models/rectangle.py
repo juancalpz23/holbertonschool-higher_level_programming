c720564af4f78d0fd0334484fc63bf22c24cacfd
Juan C Lopez <juancalpz@yahoo.com>
2024-02-15 11:04:16 -0400

    Added update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -94,3 +94,16 @@ class Rectangle(Base):
         """Function returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format
                 (self.id, self.__x, self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        """Assign an argument to each attribute"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x =args[3]
+        if len(args) >= 5:
+            self.y = args[4]
