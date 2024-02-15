a742f6c0ebbed20636e82a8c68b45a515502ef22
Juan C Lopez <juancalpz@yahoo.com>
2024-02-15 11:26:52 -0400

    Updated the update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,15 +95,14 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format
                 (self.id, self.__x, self.__y, self.__width, self.__height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assign an argument to each attribute"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        attr = ["id", "width", "height", "x", "y"]
+        _len = len(attr)
+        if len(args) > 0:
+            for i, argv in enumerate(args):
+                if i < _len:
+                    self.__setattr__(attr[1], argv)
+        else:
+            for k, v in kwargs.items():
+                self.__setattr__(k, v)
