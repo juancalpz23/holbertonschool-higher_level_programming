c95679e74b5e92c6e077697605febbf9fecccd93
Juan C Lopez <juancalpz@yahoo.com>
2024-02-15 13:02:44 -0400

    Added json string rep to base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Module containing the Base class
 """
+import json
 
 
 class Base:
@@ -19,3 +20,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
