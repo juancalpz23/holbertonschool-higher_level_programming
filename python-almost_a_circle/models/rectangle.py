88173bac86e84a366a7dcb8862fa023a64690f0e
Juan C Lopez <juancalpz@yahoo.com>
2024-02-15 11:06:58 -0400

    Pycodestyle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,6 +104,6 @@ class Rectangle(Base):
         if len(args) >= 3:
             self.height = args[2]
         if len(args) >= 4:
-            self.x =args[3]
+            self.x = args[3]
         if len(args) >= 5:
             self.y = args[4]
