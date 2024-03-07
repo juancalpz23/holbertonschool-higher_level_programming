8e1d92e1a26ce9168b2d8eed9121e507fc835c03
Juan C Lopez <juancalpz@yahoo.com>
2024-03-07 15:49:05 -0400

    Added file 1-create_user.sql - user with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create a mysql server user
+CREATE USER 
+IF NOT EXIST user_0d_1@localhost 
+IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES 
+ON * . * 
+TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
