d890bae4b53b0852a8083e27f31eb46c505d5239
Juan C Lopez <juancalpz@yahoo.com>
2024-03-07 17:03:28 -0400

    missing id properties

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create a database and table\
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL
 );
