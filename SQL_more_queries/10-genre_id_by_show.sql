62044779335b1bd47ecd46cc9b3e5c9ba22daa41
Juan C Lopez <juancalpz@yahoo.com>
2024-03-07 17:58:27 -0400

    Checker

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 --Show liked genres INNER JOIN
-SELECT a.`title`, b.`genre_id`
-FROM `tv_shows` AS a
-INNER JOIN `tv_show_genres` AS b
-ON a.`id` = b.`show_id`
-ORDER BY a.`title`, b.`genre_id`;
+SELECT a.title, b.genre_id
+FROM tv_shows AS a
+INNER JOIN tv_show_genres AS b
+ON a.id = b.show_id
+ORDER BY a.title, b.genre_id;
