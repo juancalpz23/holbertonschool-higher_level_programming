497c0932fcdafec0daeea2f9ffb8311d3d178004
Juan C Lopez <juancalpz@yahoo.com>
2024-03-07 15:55:05 -0400

    Checkers

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Create a mysql server user
-CREATE USER 
-    IF NOT EXIST user_0d_1@localhost 
+CREATE USER
+    IF NOT EXISTS user_0d_1@localhost
     IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES 
-    ON * . * 
-    TO user_0d_1@localhost
-    IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO user_0d_1@localhost
+   IDENTIFIED BY user_0d_1_pwd;
 FLUSH PRIVILEGES;
