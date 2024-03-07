5aac5ec1baf5a9e19450c0685d4c8d55c851b75a
Juan C Lopez <juancalpz@yahoo.com>
2024-03-07 17:50:51 -0400

    Checker

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 --Show liked genres Left Join
-SELECT a.title, b.genre_id 
-FROM tv_show_genres b
-LEFT JOIN tv_shows a
-ON b.show_id = a.id 
-ORDER BY a.title, b.genre_id ASC;
+SELECT b.title, a.genre_id 
+FROM tv_show_genres a 
+LEFT JOIN tv_shows b 
+ON a.show_id = b.id 
+ORDER BY b.title, a.genre_id ASC;
