2d8e82728a44d5d989e42569bce749609fae2865
Juan C Lopez <juancalpz@yahoo.com>
2024-03-07 18:09:50 -0400

    Checkers

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- List shows in database RIGHT JOIN
-SELECT a.title, b.genre_id 
-FROM tv_show_genres a
-RIGHT JOIN tv_shows b
-ON b.show_id = a.id
-ORDER BY a.title, b.genre_id ASC;
+SELECT b.title, a.genre_id 
+FROM tv_show_genres a 
+RIGHT JOIN tv_shows b 
+ON a.show_id = b.id 
+ORDER BY b.title, a.genre_id ASC;
