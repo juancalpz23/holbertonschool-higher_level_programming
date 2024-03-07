e7e875bc0be8046bc543726c39b93e6460597578
Juan C Lopez <juancalpz@yahoo.com>
2024-03-07 15:52:47 -0400

    Identified user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
 -- Create a mysql server user
 CREATE USER 
-IF NOT EXIST user_0d_1@localhost 
-IDENTIFIED BY user_0d_1_pwd;
+    IF NOT EXIST user_0d_1@localhost 
+    IDENTIFIED BY user_0d_1_pwd;
 GRANT ALL PRIVILEGES 
-ON * . * 
-TO user_0d_1@localhost;
+    ON * . * 
+    TO user_0d_1@localhost
+    IDENTIFIED BY user_0d_1_pwd;
 FLUSH PRIVILEGES;
