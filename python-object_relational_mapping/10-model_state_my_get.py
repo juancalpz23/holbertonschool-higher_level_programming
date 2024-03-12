2a1fd70c814a0a245c57bb148553e80ca12f52b8
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 11:48:06 -0400

    Checker

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -9,6 +9,7 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]),
@@ -17,7 +18,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = (session.query(State).order_by(State.id).
-             filter(State.name.like(argv[4])).first())
+             filter(State.name.like(argv[4])).
+             first())
     if state:
         print(state.id)
     else:
