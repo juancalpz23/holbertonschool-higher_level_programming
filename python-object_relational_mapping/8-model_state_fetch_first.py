2c33d19c933fd73756238c833d9b8cb6e990aad5
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 10:34:44 -0400

    Fixed typo on close()

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,8 +11,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
@@ -24,4 +24,4 @@ if __name__ == "__main__":
     else:
         print("Nothing")
 
-    session().close()
+    session.close()
