cbaf0685b9bd163d5e6e774bf5f8f0ed5281df05
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 08:09:33 -0400

    Checker

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,11 +3,12 @@
 List all states from db hbtn_0e_usa
 Takes 3 args mysql username, mysql passwd, db name
 """
+from sys import argv
+import MySQLdb
 
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
+
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
