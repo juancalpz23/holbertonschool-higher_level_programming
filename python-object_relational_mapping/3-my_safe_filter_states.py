559d8f22e177bbba4119ebb7653d8d441fb7da74
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 12:52:17 -0400

    Pycodestyle

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,19 +5,18 @@ Safe from SQL injections
 from sys import argv
 import MySQLdb
 
+
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=argv[1], passwd=argv[2],
+                        db=argv[3])
     cur = db.cursor()
-
     check = (argv[4], )
     cur.execute("SELECT * FROM states WHERE name = %s\
     ORDER BY states.id ASC", check)
-
     lst = cur.fetchall()
     for r in lst:
         print(r)
-
     cur.close()
     db.close()
