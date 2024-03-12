2f853a95d1bd23538a8b17eb8fcecf59db604c83
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 12:47:02 -0400

    Pycodestyle

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,10 +9,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", 
-                        port=3306, user=argv[1], 
+                        port=3306, user=argv[1],
                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    check = (argv[4],)
+    check = (argv[4])
     cur.execute("SELECT * FROM cities JOIN states\
     ON cities.state_id = states.id WHERE states.name = %s\
     ORDER BY cities.id ASC", check)
