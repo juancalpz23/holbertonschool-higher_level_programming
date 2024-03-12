655d2e8d2e09c3575375c5887b3e1d75181775a9
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 11:04:10 -0400

    Checker

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,14 +10,14 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = (session.query(State).order_by(State.id).first()
-    .filter(State.name.like('%a%')))
+    state = (session.query(State).order_by(State.id).
+             filter(State.name.like('%a%')))
     for states in state:
         print("{}: {}".format(states.id, states.name))
 
