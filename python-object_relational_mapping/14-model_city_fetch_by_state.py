88ab9770821f3937e79c5c3fdfcf3692993d0bbe
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 12:24:17 -0400

    Added file 14-model_city_fetch_by_state.py

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+deletes all State objects with a name
+containing the letter a from the db hbtn_0e_6_usa
+Takes 3 args
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    statescity = session.query(State, City).filter(City.state_id == State.id).all()
+    for state, city in statescity:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
