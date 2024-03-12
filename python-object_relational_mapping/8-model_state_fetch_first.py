79cce3b47e7a20f5aaaa02d8f6ad940a8f03d647
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 10:06:23 -0400

    Checker

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,24 +4,22 @@ Prints the first state obj from the db
 hbtn_0e_0_usa, takes 3 args
 """
 from sys import argv
-from model_state import State, Base
-from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
+                           .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session().query(State).order_by(State.id).first()
+    state = session.query(State).order_by(State.id).first()
 
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
 
-    session().close()
+    session.close()
