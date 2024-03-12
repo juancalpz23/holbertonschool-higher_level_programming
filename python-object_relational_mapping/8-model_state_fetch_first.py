c9395ccf3cdb0ed951f3a576016bce685913c443
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 09:48:12 -0400

    Missing property

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session().query(State).first()
-    if result:
-        print("{}: {}".format(result.id, result.name))
+    state = session().query(State).order_by(State.id).first()
+    
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
     session().close()
