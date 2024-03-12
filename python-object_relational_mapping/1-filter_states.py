238686af5a2d49e42ab09b78bf4a41fdd9c52415
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 08:42:54 -0400

    FInal checker

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,8 +3,6 @@
 List all states with a name starting with 'N'
 Should take 3 arguments username, passwd and db name
 """
-
-
 from sys import argv
 import MySQLdb
 
@@ -14,7 +12,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     
     cur = db.cursor()
-
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
                  ORDER BY id ASC;")
     
