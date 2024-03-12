0029cf892d9c2d572db78d4b1d13fbc42dd7c902
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 08:18:26 -0400

    Checkers

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,10 +11,14 @@ import MySQLdb
 if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+
     lst = cur.fetchall()
     for r in lst:
         print(r)
+
     cur.close()
     db.close()
