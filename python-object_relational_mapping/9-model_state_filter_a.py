205b416078340c68cd3ab09b4e964ff42733895c
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 11:06:29 -0400

    Checker

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-Prints the first state obj from the db
-hbtn_0e_0_usa, takes 3 args
+List all obj that contain letter a
+Takes 3 args
 """
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
-    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -20,4 +19,3 @@ if __name__ == "__main__":
              filter(State.name.like('%a%')))
     for states in state:
         print("{}: {}".format(states.id, states.name))
-
