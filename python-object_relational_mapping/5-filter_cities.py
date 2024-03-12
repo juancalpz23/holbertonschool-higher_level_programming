bc9c2c0457bd73e8b6f20e4f276d4eee6d5ea216
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 12:47:51 -0400

    Pycode

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                         port=3306, user=argv[1],
                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    check = (argv[4])
+    check = (argv[4], )
     cur.execute("SELECT * FROM cities JOIN states\
     ON cities.state_id = states.id WHERE states.name = %s\
     ORDER BY cities.id ASC", check)
