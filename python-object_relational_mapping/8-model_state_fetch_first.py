43e11fd007e53a6616dbee00c0e3edfd6bbfa6ee
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 10:45:53 -0400

    Checker

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,6 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """ Start link class to table in database """
     engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
