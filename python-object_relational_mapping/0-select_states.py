d11ba9e0a0c53e73064c1b90539ee6efca0f3ce2
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 08:06:46 -0400

    Missing connection property

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ Takes 3 args mysql username, mysql passwd, db name
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
     lst = cur.fetchall()
