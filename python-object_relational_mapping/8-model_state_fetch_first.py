d02f3aff91d138fa00fdbc19f160a371b6dfb82e
Juan C Lopez <juancalpz@yahoo.com>
2024-03-12 10:47:33 -0400

    Checks

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,11 +5,11 @@ hbtn_0e_0_usa, takes 3 args
 """
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
