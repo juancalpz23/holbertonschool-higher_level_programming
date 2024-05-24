35bd8394d718510fe490a63d054db9354201eddc
Juan C Lopez <juancalpz@yahoo.com>
2024-05-24 14:05:00 -0400

    CREATE: New file 6-script.js

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const characterElement = document.querySelector('#character');
+      characterElement.textContent = data.name;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+});
